3957d2fd31b5b369a57b12be2c2c8d91da6494fb
Olivier Truong <oliv.truong@gmail.com>
2012-11-22 17:51:41 +0100

    Adding template method pattern
    Behavioral pattern

---
+++ design-patterns/src/main/java/fr/oltruong/pattern/templatemethod/AbstractRestaurant.java
@@ -0,0 +1,67 @@
+package fr.oltruong.pattern.templatemethod;
+
+/**
+ * @author oltruong
+ */
+public abstract class AbstractRestaurant
+{
+
+    /**
+     * My template method The algorithm of this method is final. Its decided by this class. However subclasses have to
+     * implement some of the steps Method is final so that subclasses cannot change it
+     */
+    public final void haveDinner()
+
+    {
+
+        // abstract
+        orderFood();
+
+        // those 2 methods are hooks. Do nothing but can be used by subclasses
+        if ( hypeRestaurant() )
+        {
+            enjoyApetizers();
+        }
+
+        // abstract
+        haveFoodDelivered();
+
+        enjoyDinner();
+        pay();
+
+        // abstract
+        leave();
+    }
+
+    abstract void orderFood();
+
+    protected boolean hypeRestaurant()
+    {
+        // Default value : false
+        return false;
+    }
+
+    protected void enjoyApetizers()
+    {
+        // Default behavior : do nothing
+
+    }
+
+    abstract void haveFoodDelivered();
+
+    /**
+     * Method is final so that subclasses cannot change it
+     */
+    final void enjoyDinner()
+    {
+        System.out.println( "Enjoying my food with great company." );
+    }
+
+    final void pay()
+    {
+        System.out.println( "Paying with cash or credit card." );
+
+    }
+
+    abstract void leave();
+}
---
+++ design-patterns/src/main/java/fr/oltruong/pattern/templatemethod/Main.java
@@ -0,0 +1,20 @@
+package fr.oltruong.pattern.templatemethod;
+
+/**
+ * Template method is a behavioral design pattern that defines the steps of an algorithm and defers some steps to
+ * subclasses
+ * 
+ * @author oltruong
+ */
+public class Main
+{
+
+    public static void main( String[] args )
+    {
+        AbstractRestaurant classyRestaurant = new ClassyRestaurant();
+        AbstractRestaurant fastfoodRestaurant = new FastFoodRestaurant();
+
+        classyRestaurant.haveDinner();
+        fastfoodRestaurant.haveDinner();
+    }
+}
