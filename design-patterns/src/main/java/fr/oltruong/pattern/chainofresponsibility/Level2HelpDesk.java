0bf36adf9dc1d0a6db6cbc286002d48ee71b74d7
unknown <m405640@AFR563947.france.ad.airfrance.fr>
2012-12-06 18:26:54 +0100

    Add chain of responsibility pattern

---
+++ design-patterns/src/main/java/fr/oltruong/pattern/chainofresponsibility/Level2HelpDesk.java
@@ -0,0 +1,22 @@
+package fr.oltruong.pattern.chainofresponsibility;
+
+/**
+ * @author oltruong
+ */
+public class Level2HelpDesk
+    extends HelpDesk
+{
+
+    public Level2HelpDesk( int severity )
+    {
+        super( severity );
+    }
+
+    @Override
+    void doTheJob()
+    {
+        System.out.println( "This is a level2 situation" );
+
+    }
+
+}
