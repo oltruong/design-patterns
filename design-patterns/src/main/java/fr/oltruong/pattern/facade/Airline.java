aadbec832e2a9d0712f4a367b6e02667e298f60c
Olivier <oliv.truong@gmail.com>
2012-12-07 10:00:24 +0100

    Add Facade pattern

---
+++ design-patterns/src/main/java/fr/oltruong/pattern/facade/Airline.java
@@ -0,0 +1,16 @@
+package fr.oltruong.pattern.facade;
+
+public class Airline
+{
+
+    public void bookTicket()
+    {
+        System.out.println( "Booking airline ticket" );
+    }
+
+    public void cancelTicket()
+    {
+        System.out.println( "Canceling airline ticket" );
+    }
+
+}
